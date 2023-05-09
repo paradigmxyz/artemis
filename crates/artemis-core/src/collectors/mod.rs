874363f17be8308a25815dba74a886372c0037ed
pistomat <pistomat@users.noreply.github.com>
2023-05-09 18:03:37 +0200

    Add basic event log collector (#12)

--- crates/artemis-core/src/collectors/mod.rs
+++ crates/artemis-core/src/collectors/mod.rs
@@ -5,6 +5,9 @@
 /// This collector listens to a stream of new blocks.
 pub mod block_collector;
 
+/// This collector listens to a stream of new event logs.
+pub mod log_collector;
+
 /// This collector listens to a stream of new pending transactions.
 pub mod mempool_collector;
 
