469b404c0019dc2b6116892f4a7b8cd0d8ea8f03
frankie <88221686+FrankieIsLost@users.noreply.github.com>
2023-07-26 14:05:06 -0400

    types

--- crates/artemis-core/src/executors/mev_share_executor.rs
+++ crates/artemis-core/src/executors/mev_share_executor.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+
 
 use crate::types::Executor;
 use anyhow::Result;
@@ -6,49 +6,36 @@ use async_trait::async_trait;
 use ethers::signers::Signer;
 use futures::{stream, StreamExt};
 use jsonrpsee::http_client::{
-    transport::{Error as HttpError, HttpBackend},
-    HttpClient, HttpClientBuilder,
+    transport::{self}, HttpClientBuilder,
 };
-use mev_share::rpc::{FlashbotsSigner, FlashbotsSignerLayer, MevApiClient, SendBundleRequest};
-use tower::{util::MapErr, ServiceBuilder};
+use mev_share::rpc::{FlashbotsSignerLayer, MevApiClient, SendBundleRequest};
+
 use tracing::{error, info};
 
 /// An executor that sends bundles to the MEV-share Matchmaker.
-pub struct MevshareExecutor<S> {
-    mev_share_client: AuthedClient<S>,
+pub struct MevshareExecutor {
+    mev_share_client: Box<dyn MevApiClient + Send + Sync>,
 }
 
-type MapErrorFn = fn(Box<dyn Error + Send + Sync + 'static>) -> HttpError;
-type AuthedClient<S> = HttpClient<MapErr<FlashbotsSigner<S, HttpBackend>, MapErrorFn>>;
-
-impl<S: Signer + Clone + 'static> MevshareExecutor<S> {
-    pub fn new(signer: S) -> Self {
+impl MevshareExecutor {
+    pub fn new(signer: impl Signer + Clone + 'static) -> Self {
         // Set up flashbots-style auth middleware
-        let signing_middleware = FlashbotsSignerLayer::new(signer);
-        let service_builder = ServiceBuilder::new()
-            // map signer errors to http errors
-            .map_err(Self::map_box_to_http_error as MapErrorFn)
-            .layer(signing_middleware);
-
-        // Set up the rpc client
-        let url = "https://relay.flashbots.net:443";
-        let client = HttpClientBuilder::default()
-            .set_middleware(service_builder)
-            .build(url)
-            .expect("Failed to create http client");
-
+        let http = HttpClientBuilder::default()
+            .set_middleware(
+                tower::ServiceBuilder::new()
+                    .map_err(transport::Error::Http)
+                    .layer(FlashbotsSignerLayer::new(signer)),
+            )
+            .build("https://relay.flashbots.net:443")
+            .expect("failed to build HTTP client");
         Self {
-            mev_share_client: client,
+            mev_share_client: Box::new(http),
         }
-    }
-
-    pub fn map_box_to_http_error(err: Box<dyn Error + Send + Sync>) -> HttpError {
-        HttpError::Http(err)
     }
 }
 
 #[async_trait]
-impl<S: Signer + Clone + 'static> Executor<Vec<SendBundleRequest>> for MevshareExecutor<S> {
+impl Executor<Vec<SendBundleRequest>> for MevshareExecutor {
     /// Send bundles to the matchmaker.
     async fn execute(&self, action: Vec<SendBundleRequest>) -> Result<()> {
         let bodies = stream::iter(action)
